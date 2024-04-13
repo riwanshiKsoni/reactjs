1aba6a84c4faabbf7bdd9b984fcde94bf5022ce2
AmanKrSoni <amanksonimgs@gmail.com>
2024-04-13 06:20:43 +0530

    React_Project

---
+++ public/React-Project_practices/03bgChanger/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+
+
+function App() {
+const [color , setColor] = useState("olive")
+return (
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}  > 
+    <div className="fixed   flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="flex flex-wrap justify-center gap-5 shadow-lg bg-white px-3 py-2 rounded-3x1">
+
+    <button onClick={()=>  setColor("red") }  className="outline-none py-1 px-4 rounded-full shadow-lg text-white" style={{backgroundColor:"red"}} >Red</button>
+    <button onClick={()=> setColor("blue")}   className="outline-none py-1 px-4 rounded-full shadow-lg text-white" style={{backgroundColor:"blue"}}>Blue </button>
+    <button onClick={()=> setColor("pink")}   className="outline-none py-1 px-4 rounded-full shadow-lg text-white" style={{backgroundColor:"pink"}}> Pink</button>
+    <button onClick={()=> setColor("green")}   className="outline-none py-1 px-4 rounded-full shadow-lg text-white" style={{backgroundColor:"green"}}>Green </button>
+    <button onClick={()=> setColor("purple")}   className="outline-none py-1 px-4 rounded-full shadow-lg text-white" style={{backgroundColor:"purple"}}> Purple</button>
+
+
+</div>
+
+    </div>
+    </div>
+  )
+}
+
+
+ export default App
