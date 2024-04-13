04aeb5c165c0059a51db8bf767e744838edb57c1
AmanKrSoni <amanksonimgs@gmail.com>
2024-04-13 06:49:55 +0530

    changes

--- public/React-Project_practices/03bgChanger/src/App.jsx
+++ public/React-Project_practices/03bgChanger/src/App.jsx
@@ -4,11 +4,12 @@ import { useState } from "react"
 
 function App() {
 const [color , setColor] = useState("olive")
+// document.body.style.backgroundColor = color
 return (
     <div className="w-full h-screen duration-200"
     style={{backgroundColor: color}}  > 
     <div className="fixed   flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-      <div className="flex flex-wrap justify-center gap-5 shadow-lg bg-white px-3 py-2 rounded-3x1">
+      <div className="flex  rounded-3x1 flex-wrap justify-center gap-5 shadow-lg bg-white px-3 py-2 ">
 
     <button onClick={()=>  setColor("red") }  className="outline-none py-1 px-4 rounded-full shadow-lg text-white" style={{backgroundColor:"red"}} >Red</button>
     <button onClick={()=> setColor("blue")}   className="outline-none py-1 px-4 rounded-full shadow-lg text-white" style={{backgroundColor:"blue"}}>Blue </button>
